58b7921af936e25055c432183d1d5891158806ea
kentatanizawa1002 <tanizawa@blankr.fun>
2024-10-05 18:02:35 +0100

    fix profile-page

---
+++ layouts/default.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Top",
+          to: "/",
+        },
+      ],
+      miniVariant: false,
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+const toExternalLink = (link: string) => {
+  window.open(link, "_blank");
+};
+</script>
+
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6" class="contents text-center">
+      <v-img
+        src="/profile-icon.png"
+        alt="profile-image"
+        height="100"
+        width="100"
+        class="rounded-circle mx-auto"
+      />
+      <p class="display-1 font-weight-bold">Kenta Tanizawa</p>
+      <v-card class="pa-12 d-flex flex-column align-center mt-4">
+        <v-btn
+          color="#1E88E5"
+          width="120"
+          nuxt
+          class="text-capitalize"
+          @click="toExternalLink('https://github.com/ktanizaw')"
+        >
+          GitHub
+        </v-btn>
+        <v-btn
+          color="#1E88E5"
+          width="120"
+          nuxt
+          class="text-capitalize mt-4"
+          @click="
+            toExternalLink(
+              'https://www.linkedin.com/in/kenta-tanizawa-775b5226b/'
+            )
+          "
+        >
+          LinkedIn
+        </v-btn>
+        <v-btn
+          color="#1E88E5"
+          width="120"
+          nuxt
+          class="text-capitalize mt-4"
+          @click="toExternalLink('https://x.com/kents_7')"
+        >
+          X
+        </v-btn>
+        <v-btn
+          color="#1E88E5"
+          width="120"
+          nuxt
+          class="text-capitalize mt-4"
+          @click="toExternalLink('https://qiita.com/kents1002')"
+        >
+          Qiita
+        </v-btn>
+        <v-btn
+          color="#1E88E5"
+          width="120"
+          nuxt
+          class="text-capitalize mt-4"
+          @click="toExternalLink('https://medium.com/@kents1002')"
+        >
+          Medium
+        </v-btn>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.contents {
+  padding-top: 30px;
+}
+.button-text {
+  text-transform: none; /* 大文字変換を無効化 */
+}
+</style>
